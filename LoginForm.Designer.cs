8719ec2d059a52e5deca1e62bb7327efd4722864
maskey <madaskay.arm@gmail.com>
2025-03-21 20:23:28 -0500

    added database

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.Enter = new System.Windows.Forms.Button();
+            this.Edit = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -40,51 +40,51 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(231, 92);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(116, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(231, 187);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(231, 176);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(116, 20);
             this.textBox2.TabIndex = 1;
             // 
-            // button1
+            // Enter
             // 
-            this.button1.Location = new System.Drawing.Point(262, 261);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Enter";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Enter.Location = new System.Drawing.Point(262, 261);
+            this.Enter.Margin = new System.Windows.Forms.Padding(2);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(56, 19);
+            this.Enter.TabIndex = 2;
+            this.Enter.Text = "Enter";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Edit_Click);
             // 
-            // button2
+            // Edit
             // 
-            this.button2.Location = new System.Drawing.Point(106, 261);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(56, 19);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.Edit.Location = new System.Drawing.Point(106, 261);
+            this.Edit.Margin = new System.Windows.Forms.Padding(2);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(56, 19);
+            this.Edit.TabIndex = 3;
+            this.Edit.Text = "Exit";
+            this.Edit.UseVisualStyleBackColor = true;
+            this.Edit.Click += new System.EventHandler(this.Exit_Click_1);
             // 
-            // button3
+            // Clear
             // 
-            this.button3.Location = new System.Drawing.Point(431, 261);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(56, 19);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Clear";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            this.Clear.Location = new System.Drawing.Point(431, 261);
+            this.Clear.Margin = new System.Windows.Forms.Padding(2);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(56, 19);
+            this.Clear.TabIndex = 4;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click_1);
             // 
             // label1
             // 
@@ -98,7 +98,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(260, 172);
+            this.label2.Location = new System.Drawing.Point(260, 161);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 6;
@@ -111,12 +111,12 @@
             this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Edit);
+            this.Controls.Add(this.Enter);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "LoginForm";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -129,9 +129,9 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.Button Edit;
+        private System.Windows.Forms.Button Clear;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
     }
---
+++ LoginForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Ujwal_Test;
+
+namespace WindowsFormsApp1
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Edit_Click(object sender, EventArgs e)
+        {
+            if (isValid())
+            {
+                string connString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\ARNAVCS\RetirementCalc.mdb;";
+                Boolean validUser = false;
+
+                using (OleDbConnection conn = new OleDbConnection(connString))
+                {
+                    conn.Open();
+                    OleDbCommand cmd = new OleDbCommand("SELECT * FROM login_information", conn);
+                    OleDbDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        //Console.WriteLine(reader["ColumnName"].ToString());
+                        string username = reader["username"].ToString();
+                        string password = reader["password"].ToString();
+                        if (username == textBox1.Text && password == textBox2.Text)
+                        {
+                            validUser = true;
+                            MessageBox.Show("Login Successful", "Success");
+                            EditValues form2 = new EditValues();
+                            form2.Show();
+                            this.Hide();
+                            return;
+                        }
+
+                    }
+                    if (validUser== false)
+                    {
+                        MessageBox.Show("Invalid Credentials. Try again", "Error");
+                        return;
+                    }
+                }
+
+            }
+            
+        }
+
+
+        private bool isValid()
+        {
+            if (textBox1.Text.TrimStart() == string.Empty)
+            {
+                MessageBox.Show("enter valid user name", "error");
+                return false;
+            }
+            else if (textBox2.Text.TrimStart() == string.Empty)
+            {
+                MessageBox.Show("enter valid password", "error");
+                return false;
+            }
+            return true;
+        }
+
+        private void Exit_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Clear_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
